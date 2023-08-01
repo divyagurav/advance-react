7000e00d983d9744fca6922689a79169f5c1e5bd
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-08-01 13:35:31 +0530

    Add files via upload

---
+++ task10/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import ExpenseItem from "./comopnents/ExpenseItem";
+import NewExpense from "./comopnents/NewExpense";
+import ExpensesFilter from "./comopnents/ExpensesFilter";
+import "./App.css";
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(dummyExpenses);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+    console.log("in app.js");
+    console.log(expense);
+  };
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <div className="App">
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpensesFilter>
+
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        ></ExpenseItem>
+      ))}
+      {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem> */}
+    </div>
+  );
+}
+
+export default App;
